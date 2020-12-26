512ef4b97bd0f342bf63353ca8dccf1036a384fa
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-12-26 19:56:59 +0530

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    const method = req.method;
     const url = req.url;
     if(url === '/') {
         res.write('<html>');
@@ -9,6 +11,14 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();    
     }
+
+    if(url=== '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302; //code for redirection
+        res.setHeader('Location', '/'); // Location is default header
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
