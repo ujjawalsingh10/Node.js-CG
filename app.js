dfb02a95bd5833d634e7abdff624fc59bfdc4ecb
Ujjawal Singh <iamujjawal10.us@gmail.com>
2021-01-01 23:22:43 +0530

    Blocking and non-Blocking Code

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () =>{
+        return req.on('end', () =>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302; //code for redirection
+                res.setHeader('Location', '/'); // Location is default header
+                return res.end();
+            });
         });
-        res.statu1sCode = 302; //code for redirection
-        res.setHeader('Location', '/'); // Location is default header
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
